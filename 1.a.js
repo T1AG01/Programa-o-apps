099a01506c4f0619f2be2504ac3ea7ee10e23a0f
T1AG01 <tiagotlucena@gmail.com>
2025-07-29 08:41:02 -0300

    a

---
+++ 1.a.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Digite seu nome: ");
+let idade = prompt("Digite sua idade: ")
+let altura = prompt("Digite sua altura: ")
+console.log("Meu nome é",nome,", tenho",idade,"anos e",altura,"m de altura")
