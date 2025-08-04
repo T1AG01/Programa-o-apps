e7b9333bb890772f9f08e17737af4be4d570ec77
T1AG01 <135227289+T1AG01@users.noreply.github.com>
2025-08-04 11:28:41 -0300

    Add files via upload

---
+++ Questão_3/3a.js
@@ -0,0 +1,8 @@
+let xp = 0;
+
+for (let batalha = 1; batalha <= 10; batalha++) {
+    console.log(`Batalha ${batalha}: Você ganhou 10 pontos de experiência!`);
+    xp = xp + 10;
+}
+
+console.log(`Experiência total: ${xp}`);
