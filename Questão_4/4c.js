93c70a00897d3e924c7c40bed779d7dcee11a941
T1AG0 <tiagotlucena@gmail.com>
2025-08-14 19:26:23 -0300

    a

--- Questão_4/4c.js
+++ Questão_4/4c.js
@@ -5,14 +5,15 @@ let notas = [];
 let notas7 = [];
 
 for (let i = 0; i < qtdAlunos; i++) {
+    let nome = prompt("Digite o nome do aluno: ");
     let nota = Number(prompt("Digite a nota do aluno: ")); 
-    notas.push(nota); // salva só o número
+    notas.push([nome, nota]);
     if (nota >= 7) {
-        notas7.push(nota); 
+        notas7.push([nome, nota]); 
     }
 }
 
-console.log("\nNotas maior ou igual a 7:");
+console.log("\nAlunos com nota maior ou igual a 7:");
 for (let f = 0; f < notas7.length; f++) {
-    console.log(notas7[f]);
+    console.log(`O aluno ${notas7[f][0]} tirou ${notas7[f][1]}`);
 }
